e6324f8478706b4cafbfc0165b32e776eabdf4b2
Sm1feb <roshni>
2023-02-05 23:07:41 +0530

    creating signup,login and logout function with auth  in usercontroller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,45 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\registerRequest;
+use App\Http\Requests\{registerRequest,loginRequest};
 use App\Models\User;
+use Hash;
+use Auth;
 
 class UsersController extends Controller
 {
      //*****************SIGN UP FUNCTION IS USED FOR REGESTRATION OF USER*************** */
      public function signUp(registerRequest $request)
      {
-        $validatedData = $request->validate([
-            'name'=>'required',
-            'email'=>'required|email|unique:users',
-            'password'=>'required',
-
-          ]);
-
-         $add = new User;
-         $add->name = $request->get('name');
-         $add->email = $request->get('email');
-         $add->password = $request->get('password');
-         $add->save();
+        User::Create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
          return redirect ('home');
      }
          //*****************LOGIN FUNCTION IS USED FOR AUTHENTICATION OF USER*************** */
-    public function login(Request $request)
+    public function login(loginRequest $request)
     {
 
-        $add = new User;
-        if ($request->isMethod('post')) {
-            $email = $request->get('email');
-            $password = $request->get('password');
-            $row = User::where(['email' => $email, 'password' => $password])->first();
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password]))
+        {
 
-            if (!empty($row)) {
-                return redirect('home');
-            } else {
-                return back()->withErrors('USERINVALID');
-            }
+           return redirect('home');
+
         }
-        return view('/home');
+        else{
+             return view('login');
+        }
     }
+     //*****************LOGOUT  FUNCTION FOR LOGOUT OF USER *************** */
+     public function logout()
+     {
+         auth::logout();
+         return redirect('home');
+     }
 
 }
